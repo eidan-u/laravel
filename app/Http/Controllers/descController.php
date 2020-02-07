90fd102e714869fa1a9b6ef35e054e29b5a871ce
Your Name <you@example.com>
2020-02-07 08:53:14 +0100

    laravel 1

---
+++ app/Http/Controllers/descController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class descConrtoller extends Controller
+{
+    public function bonjour(){
+      
+        return view('contact');
+    }
+}
