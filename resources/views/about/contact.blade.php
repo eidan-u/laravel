512a7d89487883cdda7d1b2e6e64dfc00117ad8a
eidan-u <55088091+eidan-u@users.noreply.github.com>
2020-02-09 22:37:57 +0100

    blog : accueil page + login/register + custom css page

---
+++ resources/views/about/contact.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>contact</h1>
+
+<p>Bonjour</p>
+</body>
+</html>
+@endsection
